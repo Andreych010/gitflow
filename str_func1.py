56092fe1b302331605424c41ce7a99305ebd3be4
andreych <marchuk-8282@mail.ru>
2023-03-30 10:58:51 +0200

    add str_func1.py,capitalize_first_letters

--- str_func1.py
+++ str_func1.py
@@ -1,2 +1,12 @@
 def capital_letters(word):
+    '''
+принимает на вход строку и возвращает ее со всеми заглавными буквами.
+    '''
     return word.upper()
+
+def capitalize_first_letters(word):
+    '''
+    делает заглавными первые буквы каждого слова в строке, поступившей на вход функции
+    '''
+
+    return word.title()
